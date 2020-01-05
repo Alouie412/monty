4f9c78cc6620c6c6f4dea63c7da3108930f70f61
Alouie412 <alouie412@gmail.com>
2020-01-05 01:48:21 +0000

    Removed filler function placeholder

--- line_interpreter.c
+++ line_interpreter.c
@@ -1,20 +1,6 @@
 #include "monty.h"
 
 /**
- * filler - This
- * @stack: Is
- * @line_num: A
- * Return: Filler
- */
-void filler(stack_t **stack, unsigned int line_num)
-{
-	(void) **stack;
-
-	printf("This is a filler function. ");
-	printf("Your line number is %u, by the way.\n", line_num);
-}
-
-/**
  * line_interpreter - Function that matches the command in a file with
  * the given struct and executes the respective functions, if possible
  * @stack: The stack. A linked list in stack format
@@ -38,8 +24,6 @@ void line_interpreter(stack_t **stack, char *command, unsigned int line_num)
 	{"mod", modulo_op},
 	{"pchar", print_char},
 	{"pstr", print_string},
-	{"rotl", filler},
-	{"rotr", filler},
 	{NULL, NULL}
 	};
 	int i = 0;
