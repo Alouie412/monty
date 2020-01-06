9a2990d9e05b9a00eda005631b4a94a7e708a2c7
Alouie412 <alouie412@gmail.com>
2020-01-06 21:19:24 +0000

    Added support for spaces in... bizarre places

--- read_file.c
+++ read_file.c
@@ -10,6 +10,7 @@ void read_file(char *file_name, stack_t **head)
 {
 	FILE *fp;
 	char *command = NULL;
+	char *cmd_cpy = NULL;
 	size_t len = 0;
 	int string;
 	unsigned int count = 0;
@@ -23,11 +24,12 @@ void read_file(char *file_name, stack_t **head)
 
 	while ((string = getline(&command, &len, fp)) != -1)
 	{
-		command = strtok(command, DELIMIT);
+		cmd_cpy = command;
+		cmd_cpy = strtok(cmd_cpy, DELIMIT);
 		count++;
 
 		if (command != NULL)
-			line_interpreter(head, command, count);
+			line_interpreter(head, cmd_cpy, count);
 	}
 
 	free(command);
